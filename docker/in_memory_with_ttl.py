ed1b9e8c35f15cb8f09b828679694f6018eb8c6a
Saurab Dulal <dulal.saurab@gmail.com>
2026-06-05 07:27:30 -0700

    initial

---
+++ docker/in_memory_with_ttl.py
@@ -0,0 +1,27 @@
+'''
+some of the quesiton to clarify:
+ -
+
+'''
+from threading import Lock
+import time
+
+class InMemoryStorateTTL:
+    
+    def __init__(self):
+        self._data = {}
+        self._lock = Lock()
+
+    
+    def set(self, key, value, ttl=None):
+        # lets say it was set to 5 seconds
+        expires_at = time.monotonic() + ttl if ttl else None
+        self._data[key] = (value, expires_at)
+        
+    
+    def get():
+        
+
+    def delete():
+        pass
+    
